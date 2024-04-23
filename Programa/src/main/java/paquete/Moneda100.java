a7fce183dd0d2100fb90aa41e1324c3a61934e4b
Xaidev <neclxas3311@gmail.com>
2024-04-23 16:56:35 -0400

    Docs subclases moneda

--- Programa/src/main/java/paquete/Moneda100.java
+++ Programa/src/main/java/paquete/Moneda100.java
@@ -1,10 +1,18 @@
 package paquete;
-
+/*
+* Subclase de Moneda
+* */
 public class Moneda100 extends Moneda{
+    /*
+    * Constructor de Moneda100
+    * */
     public Moneda100(){
         super();
     }
-
+    /*
+    * Obtener acceso al valor de la moneda
+    * @return valor de moneda
+    * */
     public int getValor() {
         return 100;
     }
--- Programa/src/main/java/paquete/Moneda1500.java
+++ Programa/src/main/java/paquete/Moneda1500.java
@@ -1,10 +1,18 @@
 package paquete;
-
+/*
+ * Subclase de Moneda
+ * */
 public class Moneda1500 {
+    /*
+     * Constructor de Moneda1000
+     * */
     public Moneda1500(){
         super();
     }
-
+    /*
+     * Obtener acceso al valor de la moneda
+     * @return valor de moneda
+     * */
     public int getValor(){
         return 1500;
     }
