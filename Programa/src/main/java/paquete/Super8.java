169f70e067c647765a216b47d1b3fca0561b8028
Xaidev <neclxas3311@gmail.com>
2024-04-23 19:04:49 -0400

    Docs super8

--- Programa/src/main/java/paquete/Super8.java
+++ Programa/src/main/java/paquete/Super8.java
@@ -1,12 +1,19 @@
 package paquete;
 /*
-*
-* */
+ * Subclase de Producto, Define un producto en especifico
+ * */
 public class Super8 extends Producto{
+    /*
+    * Constructor de Super8
+    * @param serie, Define la serie del producto
+    * */
     public Super8(int serie) {
         super(serie);
     }
-
+    /*
+    * Metodo sabor Define el producto que hemos consumido/comprado
+    * @return Producto
+    * */
     public String sabor() {
         return "Super8";
     }
