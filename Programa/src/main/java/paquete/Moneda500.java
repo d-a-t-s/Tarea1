03f3831fc1bfe598e652d2a9288d41f19d3b9d83
Your Name <you@example.com>
2024-04-24 19:45:44 -0400

    Agrego Monedas

---
+++ Programa/src/main/java/paquete/Moneda500.java
@@ -0,0 +1,19 @@
+package paquete;
+/*
+ * Subclase de Moneda
+ * */
+public class Moneda500 {
+    /*
+     * Constructor de Moneda500
+     * */
+    public Moneda500(){
+        super();
+    }
+    /*
+     * Obtener acceso al valor de la moneda
+     * @return valor de moneda
+     * */
+    public int getValor(){
+        return 500;
+    }
+}
